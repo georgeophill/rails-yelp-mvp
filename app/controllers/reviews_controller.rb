8dbefa85e7b39cc7c10437f85db069586c1c2266
George Hill <georgeophill@outlook.com>
2022-11-16 16:59:44 +0000

    CRUD restaurants and reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,24 @@
 class ReviewsController < ApplicationController
+before_action :set_restaurant, only: %i[ create ]
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
   def create
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 
   def set_restaurant
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :restaurants
-  resources :reviews, only: [:create]
+  resources :restaurants, only: [:create] do
+   resources :reviews, only: [:new, :create]
+  end
 end
